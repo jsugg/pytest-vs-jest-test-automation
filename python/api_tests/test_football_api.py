a9fb4d1034d319008ecd22ca8d77ff6724b2ac56
Juan Sugg <juanpedrosugg@gmail.com>
2023-03-17 09:06:02 -0300

    House-keeping

--- python/api_tests/test_football_api.py
+++ python/api_tests/test_football_api.py
@@ -1,18 +1,25 @@
+import pytest
 import requests
+import random
+import string
 
 class TestFootballAPI:
     base_url = "https://api.football-data.org/v2"
 
-    def test_competitions(self):
+    @pytest.mark.flaky(retries=3)
+    def test_response_ok(self):
         response = requests.get(f"{self.base_url}/competitions")
         assert response.status_code == 200
         assert "count" in response.json()
 
-    def test_unauthorized_request(self):
+    @pytest.mark.flaky(retries=3)
+    def test_resource_not_found(self):
         response = requests.get(f"{self.base_url}/non-existent")
         assert response.status_code == 404
 
-    def test_not_modified_request(self):
-        headers = {"X-Auth-Token": "invalid_token"}
+    @pytest.mark.flaky(retries=3)
+    def test_unauthorized_request(self):
+        random_token = "".join(random.choice(string.ascii_letters + string.digits) for _ in range(32))
+        headers = {"X-Auth-Token": random_token}
         response = requests.get(f"{self.base_url}/competitions", headers=headers)
         assert response.status_code == 400
--- python/web_tests/test_duckduckgo.py
+++ python/web_tests/test_duckduckgo.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from web_tests.pages.duckduckgo_page import DuckDuckGoPage
 
+@pytest.mark.flaky(retries=5)
 def test_duckduckgo_search(driver):
     duckduckgo_page = DuckDuckGoPage(driver)
     duckduckgo_page.load()
@@ -16,8 +17,6 @@ def test_duckduckgo_search(driver):
         if first_result:
             assert first_result.get_attribute('href') == "https://www.football-data.org/"
         else:
-            pytest.fail("No search results found.")
-    except TimeoutException:
-        pytest.fail("Timeout waiting for search results.")
-    except Exception:
-        pytest.fail("No search results found.")
+            pytest.fail("The expected website was not the first search result.")
+    except Exception as e:
+        pytest.fail(f"Caught a {type(e)}. Cause: {e.__traceback__}")
