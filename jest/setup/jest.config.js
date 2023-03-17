1ba328bd37d352e15be6793a5c5d4f22116249b6
Juan Sugg <juanpedrosugg@gmail.com>
2023-03-17 09:21:58 -0300

    Housekeeping

--- jest/setup/jest.config.js
+++ jest/setup/jest.config.js
@@ -3,10 +3,5 @@ module.exports = {
     verbose: true,
     reporters: ['default', 'jest-stare'],
     testResultsProcessor: '<rootDir>/node_modules/jest-stare',
-    //setupFilesAfterEnv: ['<rootDir>/setup/jest.setup.js'],
-    testMatch: ['<rootDir>/tests/**/*[tT]est.js'],
-    //moduleFileExtensions: [...defaults.moduleFileExtensions, 'js', 'node', 'ts', 'tsx'],
-    //testEnvironment: 'jest-circus-allure-environment',
-    //modulePaths: ['<rootDir>/tests/page_objects/**/*', '<rootDir>/tests/__tests__/*']
 }
   
